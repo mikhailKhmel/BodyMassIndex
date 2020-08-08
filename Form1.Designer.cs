0091ef2c561532d70c8947c2da26297004834575
Михаил Хмельницкий <xmelnickijma@outlook.com>
2020-08-08 19:38:52 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace BodyMassIndex
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_rost = new System.Windows.Forms.TextBox();
+            this.textBox_ves = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 35.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 64.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox_rost, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox_ves, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 1, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.FixedSize;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 90F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(407, 157);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Рост (см)";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(3, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 33);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Вес (кг)";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox_rost
+            // 
+            this.textBox_rost.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox_rost.Location = new System.Drawing.Point(147, 3);
+            this.textBox_rost.Name = "textBox_rost";
+            this.textBox_rost.Size = new System.Drawing.Size(257, 22);
+            this.textBox_rost.TabIndex = 2;
+            // 
+            // textBox_ves
+            // 
+            this.textBox_ves.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox_ves.Location = new System.Drawing.Point(147, 36);
+            this.textBox_ves.Name = "textBox_ves";
+            this.textBox_ves.Size = new System.Drawing.Size(257, 22);
+            this.textBox_ves.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 85);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Посчитать ИМТ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Location = new System.Drawing.Point(147, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(257, 91);
+            this.label3.TabIndex = 5;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 157);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Индекс массы тела";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_rost;
+        private System.Windows.Forms.TextBox textBox_ves;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BodyMassIndex
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double rost;
+            if (Double.TryParse(textBox_rost.Text, out rost))
+            {
+                double ves;
+                if (Double.TryParse(textBox_ves.Text, out ves))
+                {
+                    double bmi = ves / ((rost / 100) * (rost / 100));
+                    if (bmi <= 18.5)
+                    {
+                        label3.Text = "Ниже нормального веса";
+                        label3.BackColor = System.Drawing.Color.Red;
+                    }
+                    else if (bmi >= 18.5 && bmi < 25)
+                    {
+                        label3.Text = "Нормальный вес";
+                        label3.BackColor = System.Drawing.Color.LightGreen;
+                    }
+                    else if (bmi >= 25 && bmi < 30)
+                    {
+                        label3.Text = "Избыточный вес";
+                        label3.BackColor = System.Drawing.Color.Yellow;
+                    }
+                    else if (bmi >= 30 && bmi < 35)
+                    {
+                        label3.Text = "Ожирение I степени";
+                        label3.BackColor = System.Drawing.Color.Orange;
+                    }
+                    else if (bmi >= 35 && bmi < 40)
+                    {
+                        label3.Text = "Ожирение II степени";
+                        label3.BackColor = System.Drawing.Color.OrangeRed;
+                    }
+                    else if (bmi >= 40)
+                    {
+                        label3.Text = "Ожирение III степени";
+                        label3.BackColor = System.Drawing.Color.Red;
+                    }
+                }
+            }
+        }
+    }
+}
